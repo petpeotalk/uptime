11b8b9fb8f5e96311ccdd9d81b6ecb4f823d6163
Chanjoo Lee <lees3301@gmail.com>
2020-08-24 23:29:08 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,18 @@ try {
   const urlToHit = core.getInput('url-to-hit');
   const expectedStatuses = core.getInput('expected-statuses').split(",").map((status) => Number(status)) ;
   console.log(`Pinging ${urlToHit} and expecting ${expectedStatuses}`);
+  
+  const options = {
+    hostname: '13.124.8.141',
+    port: 2500,
+    path: '/devices/version',
+    method: 'POST',
+    headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': data.length
+  }
 
-  https.post(urlToHit, (resp) => {
+  https.request(options, (resp) => {
     if (!expectedStatuses.includes(resp.statusCode)) {
       core.setFailed(`Request status was ${resp.statusCode}`);
     } else {
